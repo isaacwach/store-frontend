e4400a39bfab5193c9bb2ce719ae14e2d8bce00a
zakiya adam <zakiyaadan12@gmail.com>
2022-04-28 00:43:30 +0300

    created login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,10 +7,22 @@ const Login = () => {
             <Container>
                 <Row>
                     <Col lg={4} md={6} sm={12}>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                            </Form.Group>
 
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                            Login
+                            </Button>
+                        </Form>
                     </Col>
                     <Col lg={8} md={6} sm={12}>
-                      
+
                     </Col>
                 </Row>
             </Container>
