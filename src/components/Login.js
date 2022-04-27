e2b05546b298c347464c787578d99c3ee5df691e
zakiya adam <zakiyaadan12@gmail.com>
2022-04-28 00:31:22 +0300

    created login file

---
+++ src/components/Login.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Col, Row, Container} from "react-bootstrap";
+
+const Login = () => {
+    return (
+        <>
+            <Container>
+                <Row>
+                    <Col lg={4} md={6} sm={12}>
+
+                    </Col>
+                    <Col lg={8} md={6} sm={12}>
+                      
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    
+    );
+};
+
+
+export default Login ;
