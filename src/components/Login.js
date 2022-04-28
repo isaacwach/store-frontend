7045532a668cc4e7b623902576056da58ebfc90b
zakiya adam <zakiyaadan12@gmail.com>
2022-04-28 15:43:34 +0300

    fixed errors

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Col, Row, Container} from "react-bootstrap";
+import './Login.css';
 import profile from "../images/profile.png"
+import uiImg from "../images/login.svg"
 import email from "../images/message.jpg"
 import pass from "../images/pass.jpg"
 
-const Login = () => {
+const LoginPage = () => {
     return (
         <>
-            <Container>
-                <Row>
-                    <Col lg={4} md={6} sm={12}>
+            <Container className="mt-5">
+        
+                <Row className="mt-5">
+                    <Col lg={4} md={6} sm={12} className="text-center p-4">
+                        <img src={profile} alt="profile" className="profile"/>
                         <Form>
                             <Form.Group className="mb-3" controlId="formBasicEmail">
                                 <Form.Label>Email address</Form.Label>
@@ -19,17 +23,25 @@ const Login = () => {
                             <Form.Group className="mb-3" controlId="formBasicPassword">
                                 <Form.Control type="password" placeholder="Password" />
                             </Form.Group>
-                            <Button variant="primary" type="submit">
+                            <div className="text-righ mt-3">
+                                <a href=""><small className="reset">Forgot Password</small></a>
+                            </div>
+                            <Button className="button-login" type="submit">
                             Login
                             </Button>
+                            <div>
+                                <p className=";link">
+                                    <p>Dont have account</p><a href="#">Sign Up</a>
+                                </p>
+                            </div>
                         </Form>
                     </Col>
                     <Col lg={8} md={6} sm={12}>
-
+                        <img className="w-100" src={uiImg} alt=""/>
                     </Col>
                 </Row>
             </Container>
-            <div className="main">
+            {/* <div className="main">
                 <div className="sub-main">
                     <div>
                         <div className="imgs">
@@ -43,16 +55,22 @@ const Login = () => {
                                 <img src={email} alt="email" className="email"/>
                                  <input type="text" placeholder="username" className="name"/> 
                             </div>
-                            <div>
-                                <img src={pass} alt="pass" className="pass"/>
-                                 <input type="text" placeholder="password" className="pass"/> 
-                                 <p>Don't have an aacount? Sign Up</p>
+                            <div className="second-input">
+                                <img src={pass} alt="pass" className="email"/>
+                                 <input type="text" placeholder="password" className="name"/> 
                             </div>
-                            <button>Login</button>
+                            <div className="login-button">
+                                <button>Login</button>
+                            </div>
+                            <div>
+                                <p className=";link">
+                                    <p>Dont have account</p><a href="#">Sign Up</a>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </>
     
     );
