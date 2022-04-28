8cf01e7dba521ec480e6121dc748855f38ec6824
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-28 22:46:56 +0300

    added social icons in the footer

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import {FaFacebook, FaInstagram, FaTwitter} from 'react-icons/fa';
 
 const Footer = () => {
     return (
         <footer>
             <div className="social-icons">
-
+               <FaFacebook/> <FaInstagram/> <FaTwitter/>
             </div>
-            <div>
+            <div className="privacy">
                 <p>Terms of Service</p>
                 <p>Privacy Policy</p>
             </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+
 import App from './App';
 import "./homepage/home.css";
+import "./footer/footer.css"
 
 ReactDom.render(<App />, document.getElementById('root'))
