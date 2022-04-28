4df25e7ac9b0f24ec31546824c746c013e2f354c
zakiya adam <zakiyaadan12@gmail.com>
2022-04-28 17:27:22 +0300

    add signup .js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Container, Navbar, NavDropdown,Nav } from 'react-bootstrap';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Login from './components/login.component'
+import SignUp from './components/signup.component'
 
 const App= () => {
     return (  
