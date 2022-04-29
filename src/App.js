301720f609003cba625808e8f20e9dbf791ebdb4
Francisflow <francisngigi948@gmail.com>
2022-04-29 17:37:45 +0300

    Test routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Navbar, NavDropdown,Nav } from 'react-bootstrap';
+import { Link, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import "./App.css"
 import Booking from "./booking/booking.js";
 import Admin from "./admin/admin.js";
@@ -8,15 +9,17 @@ import Admin from "./admin/admin.js";
 const App= () => {
     return ( 
         <> 
-        <div>
+        <div className="App">
+
         <Navbar bg="light" expand="lg">
   <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Brand href="#home">Classic store</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
+        <Link to="/">Home</Link>
+        <Link to="/admin">Admin </Link>
+        <Link to="/booking">book</Link>
 
         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -29,10 +32,19 @@ const App= () => {
     </Navbar.Collapse>
   </Container>
 </Navbar>
-<Booking />
-<Admin />
+  {/* <Router>
+    <Switch>
+      <Route exact path="/booking">
+      <Booking />
+      </Route>
+      <Route path="/admin">
+      <Admin />
+      </Route>
+</Switch>
+</Router> */}
 </div>
 </>
+
     );
 }
 
