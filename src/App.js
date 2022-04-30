67b3625280a86932ca4f2712f3478cdd2eb44b10
Francisflow <francisngigi948@gmail.com>
2022-04-30 13:17:27 +0300

    Added routing moudule

--- src/App.js
+++ src/App.js
@@ -2,19 +2,37 @@ import React from 'react';
 import Home from './homepage/homepage'
 import Footer from './footer/footer'
 import NavbarHead from './navbar/navbar'
-import { Container, Navbar, NavDropdown,Nav } from 'react-bootstrap';
 import { Link, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import "./App.css"
 import Booking from "./booking/booking.js";
 import Admin from "./admin/admin.js";
+import GetBookings from './requests/request';
 
 
 const App= () => {
     return (    
       <>
+    
 <NavbarHead />
+<Router>
+  <Switch>
+    <Route exact path='/'>
 <Home />
+</Route>
+<Route path='/booking'>
+
+<Booking />
+</ Route>
+<Route path='/admin'>
+  <Admin />
+  </Route>
+<Route path='/sbooking'>
+  <GetBookings />
+</Route>
+</Switch>
+</Router>
 <Footer />
+
       </>
         
 
