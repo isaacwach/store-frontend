b812a1cd37701747ff611b3cb01b1c3473ddbfee
zakiya adam <zakiyaadan12@gmail.com>
2022-05-02 16:13:23 +0300

    fixed errors

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -11,8 +11,8 @@ const Login = () => {
             <img src='/images/profile.png' alt='profileicon'/>
             <Form>
               <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="email" placeholder="username" />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
--- src/SignUp/SignUp.js
+++ src/SignUp/SignUp.js
@@ -10,12 +10,8 @@ const SignUp = () => {
             <img src='/images/profile.png' alt='profileicon'/>
             <Form>
               <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter First Name" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter Last Name" />
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" placeholder="Enter Username" />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
@@ -25,6 +21,10 @@ const SignUp = () => {
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Confirm Password </Form.Label>
+                <Form.Control type="password" placeholder="Confirm Password" />
               </Form.Group>
               <p className="forgot-password text-right">
                 Forgot <a href="#">password?</a>
