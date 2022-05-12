a29967eb9df8f45f31aecd4e028aec303879ae87
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-05-12 19:52:17 +0300

    Updates

---
+++ precache-manifest.892115c338f69933040f149b61c1abd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d24d8177390f881e8a9",
+    "url": "/store-frontend/static/css/main.5df0898d.chunk.css"
+  },
+  {
+    "revision": "6d24d8177390f881e8a9",
+    "url": "/store-frontend/static/js/main.9c4ad537.chunk.js"
+  },
+  {
+    "revision": "e9ef6045a10024057e39",
+    "url": "/store-frontend/static/js/runtime~main.1378f7c0.js"
+  },
+  {
+    "revision": "fa52e743e62ba0f87984",
+    "url": "/store-frontend/static/css/2.fbc4d356.chunk.css"
+  },
+  {
+    "revision": "fa52e743e62ba0f87984",
+    "url": "/store-frontend/static/js/2.0c3c5412.chunk.js"
+  },
+  {
+    "revision": "a96255dccecacf5c8fdb808fa8e1c1fe",
+    "url": "/store-frontend/static/media/signup.a96255dc.svg"
+  },
+  {
+    "revision": "8c179785603464d9de3c911a8caae4bf",
+    "url": "/store-frontend/static/media/hero.8c179785.png"
+  },
+  {
+    "revision": "cccaee906150a9f53bc26cc6d3833431",
+    "url": "/store-frontend/index.html"
+  }
+];
